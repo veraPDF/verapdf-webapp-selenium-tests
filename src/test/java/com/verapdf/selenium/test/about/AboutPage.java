7dd21027d390f8e386b5489fe7bb887a46f815cb
kovaliov_nikita92 <nikita.kovaliov@duallab.com>
2020-06-23 12:15:32 +0300

    update AboutPage

--- src/test/java/com/verapdf/selenium/test/about/AboutPage.java
+++ src/test/java/com/verapdf/selenium/test/about/AboutPage.java
@@ -1,6 +1,7 @@
 package com.verapdf.selenium.test.about;
 
 import com.verapdf.selenium.pages.BasePageTest;
+import com.verapdf.selenium.utils.Utils;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,6 +11,9 @@ import java.util.ArrayList;
 import static com.verapdf.selenium.blocks.BasePage.SELECTOR_ABOUT_PAGE_BUTTON;
 
 public class AboutPage extends BasePageTest {
+    private static final By SELECTOR_LINK_WCAG21 = By.linkText("WCAG 2.1");
+    private static final By SELECTOR_PDF_UA1 = By.linkText("PDF/UA-1");
+    private static final By SELECTOR_MATTERHORN = By.linkText("Matterhorn 1.02");
     private static final By SELECTOR_LINK_VERAPDF_GITHUB = By.linkText("veraPDF GitHub");
 
     @Test
@@ -21,6 +25,10 @@ public class AboutPage extends BasePageTest {
         ArrayList<String> newTab = new ArrayList<String>(driver.getWindowHandles());
         newTab.remove(oldTab);
         driver.switchTo().window(newTab.get(0));
+        Utils.captureScreenshot(driver, "About page");
+        Assert.assertTrue(driver.findElement(SELECTOR_LINK_WCAG21).isEnabled());
+        Assert.assertTrue(driver.findElement(SELECTOR_PDF_UA1).isEnabled());
+        Assert.assertTrue(driver.findElement(SELECTOR_MATTERHORN).isEnabled());
         Assert.assertTrue(driver.findElement(SELECTOR_LINK_VERAPDF_GITHUB).isEnabled());
     }
 }
